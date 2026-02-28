1d17aa3e2e73c56103ce7e9e23b98744baa77d73
p4rsha <parshakarimi16@gmail.com>
2026-02-28 10:04:50 +0330

    The first commit. Better late than never. Also, starting to get the hang of this thing. Tho, had AI explain what the fuck is what ... old school programmers be mad , but leveraging tools so even my dumbass can program is not that bad imo.

---
+++ functions/get_files_info.py
@@ -0,0 +1,40 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+
+    try:
+
+        wd_abs_path = os.path.abspath(working_directory)
+
+        target_dir = os.path.normpath(os.path.join(wd_abs_path, directory))
+
+        # Will be True or False
+    
+        valid_target_dir = os.path.commonpath([wd_abs_path, target_dir]) == wd_abs_path
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+    
+        #processing the valid target dir
+        dir_content = os.listdir(target_dir)
+
+        lines = []
+        for content in dir_content:
+            full_path = os.path.join(target_dir, content)
+            lines.append(f"- {content}: file_size={os.path.getsize(full_path)} bytes, is_dir={os.path.isdir(full_path)}")
+
+        return "\n".join(lines)
+    
+    except Exception as e:
+        return f"Error: {e}"
+        
+    
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,70 @@
+# For grabbing the API Key
+import os
+from dotenv import load_dotenv
+
+# To make genai client 
+from google import genai
+
+# To give roles
+
+from google.genai import types
+
+
+# To allow for prompmt in the CLI
+import argparse
+
+# Grabbing API key from env
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("API Key not found you dumbass")
+
+
+# client object
+client = genai.Client(api_key=api_key)
+
+
+
+
+#initlize parser object => Yoink it's method => Easy life
+
+parser = argparse.ArgumentParser(description= "This program is a an AI chatbot based on Gemini 2.5 Flash!")
+parser.add_argument("user_prompt", type= str, help= "The prompt sent our to Gemini")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output you dumbass")
+args = parser.parse_args()
+
+
+#doing what here???
+
+messeges = [types.Content(role="user" , parts=[(types.Part(text=args.user_prompt))])]
+
+def main():
+    print("Hello from Gemini!")
+
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents= messeges)
+
+
+    #failsafe for metadata 
+    if response.usage_metadata is None :
+        raise RuntimeError("Response Metadata not found you dumbass")
+
+
+    # meta-data about tokens
+
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    response_token_count = response.usage_metadata.candidates_token_count
+
+    
+
+    #Output block
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_token_count}")
+        print(f"Response tokens: {response_token_count}")
+
+
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
